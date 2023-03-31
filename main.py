f03428d3e16bf255cc86b9379a2dbf3b96e2c6b8
Alejo Osorio <0alessx0@gmail.com>
2023-03-31 13:48:38 -0600

    Generación de Reporte

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 import pytest
-
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
@@ -10,8 +10,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import ActionChains
-
 from Funciones.Funcion import Funciones_Globales
+from allure_commons.types import AttachmentType
 
 t =2
 
@@ -42,10 +42,34 @@ def test_caso1(num1, act, num2, re):
     driver = webdriver.Chrome(service=ser, options=op)
     f = Funciones_Globales(driver)
     f.Navegar("http://localhost/faefc3221188c0f1c520463a60c736e78708712fe86ade1d549694e0b8703772",t)
+    allure.attach(driver.get_screenshot_as_png(), name="abrir_pagina", attachment_type=AttachmentType.PNG)
     f.Click_Mixto("Xpath", num1, t)
+    allure.attach(driver.get_screenshot_as_png(), name="capturar_num1", attachment_type=AttachmentType.PNG)
     f.Click_Mixto("Xpath", act, t)
+    allure.attach(driver.get_screenshot_as_png(), name="capturar_op", attachment_type=AttachmentType.PNG)
     f.Click_Mixto("Xpath", num2, t)
+    allure.attach(driver.get_screenshot_as_png(), name="capturar_num2", attachment_type=AttachmentType.PNG)
     f.Click_Mixto("Xpath", "//button[@class='Button'][contains(.,'=')]", t)
+    allure.attach(driver.get_screenshot_as_png(), name="resultado", attachment_type=AttachmentType.PNG)
+    result = f.SEX("//input[@class='DisplayText']").get_attribute("value")
+    assert result == re
+
+@pytest.mark.run
+@pytest.mark.parametrize("num1, act, num2, re",get_Data())
+def test_caso5(num1, act, num2, re):
+    global webdriver
+    ser = Service("C:\Drivers\chromedriver.exe")
+    op = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=ser, options=op)
+    f = Funciones_Globales(driver)
+    f.Navegar("http://localhost/faefc3221188c0f1c520463a60c736e78708712fe86ade1d549694e0b8703772",t)
+    allure.attach(driver.get_screenshot_as_png(), name="abrir_navegador1", attachment_type=AttachmentType.PNG)
+    f.Click_Mixto("Xpath", num1, t)
+    allure.attach(driver.get_screenshot_as_png(), name="capturar_num3", attachment_type=AttachmentType.PNG)
+    f.Click_Mixto("Xpath", act, t)
+    allure.attach(driver.get_screenshot_as_png(), name="capturar_op1", attachment_type=AttachmentType.PNG)
+    f.Click_Mixto("Xpath", "//button[@class='Button'][contains(.,'=')]", t)
+    allure.attach(driver.get_screenshot_as_png(), name="resultado", attachment_type=AttachmentType.PNG)
     result = f.SEX("//input[@class='DisplayText']").get_attribute("value")
     assert result == re
 
